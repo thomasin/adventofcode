749ffb2f3767a6a3efef4f9ba8ce6294784732d0
thomasin <thomasinabraham@me.com>
2020-12-06 17:48:45 +0100

    Day 06.

---
+++ 2020/06/part2.hs
@@ -0,0 +1,24 @@
+{- stack script
+ --compile
+ --copy-bins
+ --resolver nightly-2020-11-28
+ --install-ghc
+ --package "base split"
+ --ghc-options=-Wall
+-}
+
+
+import qualified Data.List as List
+import qualified Data.List.Split as Split
+
+
+main :: IO ()
+main = do
+    input <- getContents
+    let groups = map lines $ Split.splitOn "\n\n" input
+    print $ sum $ map (length . questionsAnswered) groups
+
+
+questionsAnswered :: [String] -> String
+questionsAnswered = List.foldl1 List.intersect
+
