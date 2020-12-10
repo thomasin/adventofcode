dd47ae9335d5695f9da66b0ad042805a371958f8
thomasin <thomasinabraham@me.com>
2020-12-10 18:07:09 +0100

    Update to only use one loop with a right fold

--- 2020/10/part2.hs
+++ 2020/10/part2.hs
@@ -16,20 +16,14 @@ import Data.List
 main :: IO ()
 main = do
     input <- sort <$> mapMaybe readMaybe <$> lines <$> getContents
-    let branches = findBranches [] (0:input) :: [Int]
-    let pathways = head $ foldl (\t x -> (sum $ take x t):t) [1] branches :: Int
-    print $ pathways
+    let branches = countBranches (0:input) :: [Int]
+    print $ head $ branches
 
 
--- For every value in the list, return how many branches it connects to
--- i.e. [0,1,2,3,6] would return [3,2,1,1,1]. The last number is set to 1.
--- A valid branch is a number that is within 3 of the value.
-findBranches :: [Int] -> [Int] -> [Int]
-findBranches tree [] = tree
-findBranches tree (_:[]) = (1:tree)
-findBranches tree (x1:xs) =
-    let branches = foldl (\a -> (+) a . inRange x1) 0 (take 3 xs)
-    in findBranches (branches:tree) xs
-
-inRange :: Int -> Int -> Int
-inRange value x = fromEnum $ x - value < 4
+countBranches :: [Int] -> [Int]
+countBranches [] = []
+countBranches (x:xs) =
+    let branches = length $ takeWhile ((>) 4 . flip (-) x) xs
+        rest = countBranches xs
+    in if branches == 0 then 1:rest
+       else (sum $ take branches $ rest):rest
